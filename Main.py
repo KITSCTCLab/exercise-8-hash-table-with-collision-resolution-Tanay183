26d4d46ba1e48937bb32025dd7f0d7aa0e49fafd
Tanay <98169249+Tanay183@users.noreply.github.com>
2022-09-20 12:12:11 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ import re
 
 def display_hash(hashtable) -> None:
 	# Write your code here
-	for i in hashtable:
+	for i in range(len(hashtable)):
 		print(i, end = " ")
 		for j in hashtable[i]:
 			print("-->", end = " ")
